eb4a53a9e7d2c63416b1dea5cd685a4bc3ea20c9
Pearl <pearlayem@gmail.com>
2018-09-16 09:37:57 -0400

    final commit

--- http-proj/src/components/HelloWorld.vue
+++ http-proj/src/components/HelloWorld.vue
@@ -1,30 +1,10 @@
-<template>
-    <div class="hello">
-        <input type="text" v-model="input.firstname" placeholder="First Name" />
-        <button v-on:click="sendData()">Send</button>
-        <br />
-        <br />
-        <textarea>{{ response }}</textarea>
-    </div>
-</template>
-
 <script>
     import axios from "axios";
     import jquery from "jquery";
 
-    var azure_url = "https://" + "westcentralus" + ".api.cognitive.microsoft.com/vision/" + "v1.0/analyze" + "?" + "visualFeatures=Categories%2CDescription%2CColor&details=&language=en"
+    var azure_url = "https://southcentralus.api.cognitive.microsoft.com/customvision/v2.0/Prediction/6dc182f9-bfd8-4500-a0c3-5f78d07c0ab8/url"
 
     export default {
-        name: 'HelloWorld',
-        data () {
-            return {
-                ip: "",
-                input: {
-                    firstname: "",
-                },
-                response: ""
-            }
-        },
         mounted() {
             axios({ method: "GET", "url": "https://httpbin.org/ip" }).then(result => {
                 this.ip = result.data.origin;
@@ -33,9 +13,8 @@
             });
         },
         methods: {
-            sendData() {
-                axios({ method: "POST", "url": azure_url, "data": '{"url": "https://hdrexposed.zenfolio.com/img/s/v-3/p919518288-3.jpg"}', "headers": { "content-type":   "application/json",
-                                                                                                                                    "Ocp-Apim-Subscription-Key":  "b11c9fd600af44ac9a47523923a7fd17"} }).then(result => {
+            sendURLData(imgURL) {
+                axios({ method: "POST", "url": azure_url, "data": '{"url": imgURL}', "headers": { "content-type":   "application/json",                                                                                                                   "Prediction-Key":  "53465eed449a4f41bfd238b8614cd4af"} }).then(result => {
                     this.response = result.data;
                     console.log(result.data);
                 }, error => {
@@ -45,28 +24,3 @@
         }
     }
 </script>
-
-<style scoped>
-    h1, h2 {
-        font-weight: normal;
-    }
-
-    ul {
-        list-style-type: none;
-        padding: 0;
-    }
-
-    li {
-        display: inline-block;
-        margin: 0 10px;
-    }
-
-    a {
-        color: #42b983;
-    }
-
-    textarea {
-        width: 600px;
-        height: 200px;
-    }
-</style>
